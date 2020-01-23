6b6dd91017d41e5c07b9b1814aed124767fb516b
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2020-01-23 13:48:20 +0530

    added a silly api to integrate swagger

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
-// app.js
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'Hello World!';
+// dummy db
+let db = require('./data');
+
+app.use(
+  bodyParser({
+    enableTypes: ['json', 'form'],
+    formLimit: '10mb',
+    jsonLimit: '10mb',
+  }),
+);
+
+// start of routes
+router.get('/cats', ctx => {
+  ctx.body = db;
 });
+
+router.get('/cats/:id', ctx => {
+  ctx.body = db.find(c => c.id == ctx.params.id);
+});
+
+router.post('/cats', ctx => {
+  const new_id = db.length;
+  const new_cat = {
+    id: new_id,
+    name: ctx.request.body.name,
+  };
+  db.push(new_cat);
+
+  ctx.body = new_cat;
+});
+
+router.put('/cats/:id', ctx => {
+  const index = db.findIndex(c => c.id == ctx.params.id);
+  db[index]['name'] = ctx.request.body.name;
+
+  ctx.body = db[index];
+});
+
+router.delete('/cats/:id', ctx => {
+  db = db.filter(c => c.id != ctx.params.id);
+
+  ctx.body = {};
+});
+
+// end of routes
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(3000);
+
+app.listen(5000);
---
+++ data.js
@@ -0,0 +1,7 @@
+const cats = [
+  { id: 1, name: 'jim' },
+  { id: 2, name: 'minato' },
+  { id: 3, name: 'rin' },
+];
+
+module.exports = cats;
